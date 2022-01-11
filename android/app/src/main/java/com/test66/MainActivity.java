ec9bd8df5f371de1471e95827d27322c850b3c6f
ACOSW <acosw@yandex.ru>
2022-01-11 13:15:18 +0300

    initial

---
+++ android/app/src/main/java/com/test66/MainActivity.java
@@ -0,0 +1,6 @@
+package com.test66;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+import {Navigation} from 'react-native-navigation';
+import App from './App';
+
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.myApp.WelcomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
